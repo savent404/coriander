39426818496917e7d1120c65c04030e23b7fe475
Savent Gate <savent_gate@outlook.com>
2023-07-31 12:42:10 +0800

    backend(zephyr): appstatus adapt any board

--- app/src/coriander/backends/zephyr/appstatus.cc
+++ app/src/coriander/backends/zephyr/appstatus.cc
@@ -8,11 +8,15 @@
  *
  */
 #include "coriander/application/appstatus.h"
+
 #include <zephyr/drivers/led.h>
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(appstatus, 2);
 
 #define APP_LED DT_CHOSEN(usr_led)
-#define APP_LED_DEVICE DEVICE_DT_GET(APP_LED)
+#define APP_LED_DEVICE DEVICE_DT_GET_OR_NULL(APP_LED)
 
 #define APP_LED_BLINK_PERIOD 250
 
@@ -23,52 +27,65 @@ using Status = coriander::AppStatus::Status;
 static Status s_status = Status::Busy;
 static int s_on = 0;
 
-static void AppStatusPeriodCallback(struct k_timer* timer)
-{
-    int on = 1;
+static bool has_led() {
+  if (!APP_LED_DEVICE || !device_is_ready(APP_LED_DEVICE)) {
+    return false;
+  }
+  return true;
+}
 
-    switch (s_status) {
+static void AppStatusPeriodCallback(struct k_timer* timer) {
+  int on = 1;
+
+  if (!has_led()) {
+    return;
+  }
+
+  switch (s_status) {
     case Status::Ok:
-        on = s_on & 4;
-        break;
+      on = s_on & 4;
+      break;
     case Status::Busy:
-        on = s_on & 1;
-        break;
+      on = s_on & 1;
+      break;
     case Status::Error:
-        on = 1;
-        break;
-    }
+      on = 1;
+      break;
+  }
 
-    if (on) {
-        led_on(APP_LED_DEVICE, 0);
-    } else {
-        led_off(APP_LED_DEVICE, 0);
-    }
+  if (on) {
+    led_on(APP_LED_DEVICE, 0);
+  } else {
+    led_off(APP_LED_DEVICE, 0);
+  }
 
-    s_on++;
+  s_on++;
 }
 
 K_TIMER_DEFINE(app_status, AppStatusPeriodCallback, NULL);
 }
-} // namespace
+}  // namespace
 
 namespace coriander {
 
-AppStatus::AppStatus() noexcept
-{
-    led_off(APP_LED_DEVICE, 0);
-    k_timer_start(&app_status, K_MSEC(APP_LED_BLINK_PERIOD), K_MSEC(APP_LED_BLINK_PERIOD));
+AppStatus::AppStatus() noexcept {
+  if (!has_led()) {
+    LOG_ERR("LED device not found");
+    return;
+  }
+  led_off(APP_LED_DEVICE, 0);
+  k_timer_start(&app_status, K_MSEC(APP_LED_BLINK_PERIOD),
+                K_MSEC(APP_LED_BLINK_PERIOD));
 }
 
-void AppStatus::setStatus(Status status) noexcept
-{
-    s_status = status;
-    led_on(APP_LED_DEVICE, 0);
+void AppStatus::setStatus(Status status) noexcept {
+  if (!has_led()) {
+    return;
+  }
+  s_status = status;
+  led_on(APP_LED_DEVICE, 0);
 }
 
-AppStatus::Status AppStatus::getStatus() const noexcept
-{
-    return s_status;
-}
+AppStatus::Status AppStatus::getStatus() const noexcept { return s_status; }
 
-}
+}  // namespace coriander
