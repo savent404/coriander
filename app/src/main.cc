96355aac90fc12124765fc5cb17e3214747a167c
Savent Gate <savent_gate@outlook.com>
2023-08-02 21:49:25 +0800

    td2kw: use nvs to count reboot times

--- app/src/main.cc
+++ app/src/main.cc
@@ -16,6 +16,10 @@
 #include "coriander/coriander.h"
 
 // backends
+#include <zephyr/drivers/flash.h>
+#include <zephyr/fs/nvs.h>
+#include <zephyr/storage/flash_map.h>
+
 #include "zephyr_appstatus.h"
 #include "zephyr_diagnosis.h"
 #include "zephyr_logger.h"
@@ -32,6 +36,55 @@ static auto zephyr_backends_bindings() {
       di::bind<coriander::base::ILogger>.to<coriander::base::zephyr::Logger>(),
       di::bind<coriander::application::IAppStatus>.to<coriander::application::zephyr::AppStatus>());
 }
+
+static int get_reboot_times() {
+  struct nvs_fs fs;
+  struct flash_pages_info info;
+  int rc, count;
+
+#define NVS_PARTITION nvs_partition
+  fs.flash_device = FIXED_PARTITION_DEVICE(NVS_PARTITION);
+  fs.offset = FIXED_PARTITION_OFFSET(NVS_PARTITION);
+
+  if (!device_is_ready(fs.flash_device)) {
+    LOG_ERR("Flash device %s is not ready", fs.flash_device->name);
+    return -1;
+  }
+
+  rc = flash_get_page_info_by_offs(fs.flash_device, fs.offset, &info);
+  if (rc) {
+    LOG_ERR("Unable to get page info");
+    return -1;
+  }
+  fs.sector_size = info.size;
+  fs.sector_count = FIXED_PARTITION_SIZE(NVS_PARTITION) / info.size;
+
+  rc = nvs_mount(&fs);
+  if (rc) {
+    LOG_ERR("Unable to mount nvs");
+    return -1;
+  }
+
+  rc = nvs_read(&fs, 0, &count, sizeof(rc));
+
+  if (rc > 0) {
+    count = count + 1;
+  } else {
+    LOG_WRN("Unable to read reboot count, assuming 0");
+    count = 0;
+  }
+
+  rc = nvs_write(&fs, 0, &count, sizeof(rc));
+  if (rc != sizeof(count)) {
+    LOG_ERR("Unable to write reboot count");
+    return -1;
+  }
+
+  LOG_INF("Free space on nvs: 0x%x", nvs_calc_free_space(&fs));
+
+  return count;
+}
+
 }  // namespace
 
 int main(void) {
@@ -41,6 +94,7 @@ int main(void) {
   auto os = coriander::base::LoggerStream(
       injector.create<std::shared_ptr<coriander::base::ILogger>>());
   os << "Coriander version " << APP_VERSION_STRING << std::endl;
+  os << "Reboot times: " << get_reboot_times() << std::endl;
 
   auto diag_regsiter =
       coriander::application::zephyr::DiagnosisRegister::getInstance();
