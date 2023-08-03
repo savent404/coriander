b2c71f0fa26785317cb5b700a5d6631cf8a4a487
savent404 <savent_gate@outlook.com>
2023-08-03 07:40:29 +0000

    motorctl: introduce isensor

---
+++ app/inc/coriander/motorctl/isensor.h
@@ -0,0 +1,47 @@
+/**
+ * @file isensor.h
+ * @author Savent Gate (savent_gate@outlook.com)
+ * @brief
+ * @date 2023-08-03
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#pragma once
+
+namespace coriander {
+namespace motorctl {
+
+/**
+ * @brief interface for sensor
+ *
+ * @note put init routine into enable(), when disable()/enable() is called, the
+ * sensor has changes to update internal vars just like parameters outside.
+ */
+struct ISensor {
+  virtual ~ISensor() = default;
+
+  virtual void enable() = 0;
+  virtual void disable() = 0;
+  virtual void sync() = 0;
+
+  virtual bool needSync(unsigned syncId) {
+    if (this->syncId < syncId) {
+      this->syncId = syncId;
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * @brief set the sensor to zero, calibrate offset
+   */
+  virtual void calibrate() = 0;
+  virtual bool needCalibrate() = 0;
+
+ protected:
+  unsigned syncId = 0;
+};
+
+}  // namespace motorctl
+}  // namespace coriander
---
+++ app/tests/ut_isensor.cc
@@ -0,0 +1,38 @@
+/**
+ * @file ut_isensor.cc
+ * @author Savent Gate (savent_gate@outlook.com)
+ * @brief
+ * @date 2023-08-03
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include <gtest/gtest.h>
+
+#include "coriander/motorctl/isensor.h"
+
+using namespace coriander::motorctl;
+
+struct dummySensor : public ISensor {
+  virtual void enable() {}
+  virtual void disable() {}
+  virtual void sync() { mCnt++; }
+  virtual void calibrate() { mCnt = 0; }
+  virtual bool needCalibrate() { return mCnt < 0; }
+  int mCnt = -1;
+};
+TEST(ISensor, dummy) {
+  auto dummy = new dummySensor();
+  ISensor* sensor = dummy;
+  ASSERT_EQ(sensor->needCalibrate(), true);
+  sensor->calibrate();
+  ASSERT_EQ(sensor->needCalibrate(), false);
+  ASSERT_EQ(dummy->mCnt, 0);
+
+  ASSERT_TRUE(sensor->needSync(1));
+  ASSERT_FALSE(sensor->needSync(1));
+  ASSERT_TRUE(sensor->needSync(2));
+
+  sensor->sync();
+  ASSERT_EQ(dummy->mCnt, 1);
+}
