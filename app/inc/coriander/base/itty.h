eed033febfc73b6f75fa047c2e26b28739a52037
Savent Gate <savent_gate@outlook.com>
2023-08-19 23:00:26 +0800

    coriander: add interface itty

---
+++ app/inc/coriander/base/itty.h
@@ -0,0 +1,23 @@
+/**
+ * @file itty.h
+ * @author Savent Gate (savent_gate@outlook.com)
+ * @brief
+ * @date 2023-08-19
+ *
+ * Copyright 2023 savent_gate
+ *
+ */
+#pragma once
+
+#include <cstddef>
+
+namespace coriander {
+namespace base {
+
+struct ITty {
+  virtual ssize_t read(uint8_t* buf, size_t size) = 0;
+  virtual ssize_t write(uint8_t* buf, size_t size) = 0;
+};
+
+}  // namespace base
+}  // namespace coriander
