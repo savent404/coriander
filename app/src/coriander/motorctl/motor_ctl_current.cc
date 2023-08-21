88f2d5ae9450ae34789aa0270f03cff7ed1b1755
Savent Gate <savent_gate@outlook.com>
2023-08-21 23:31:00 +0800

    motorctl: introduce current controller

---
+++ app/src/coriander/motorctl/motor_ctl_current.cc
@@ -0,0 +1,102 @@
+/**
+ * @file motor_ctl_current.cc
+ * @author Savent Gate (savent_gate@outlook.com)
+ * @brief
+ * @date 2023-08-21
+ *
+ * Copyright 2023 savent_gate
+ *
+ */
+#include "coriander/motorctl/motor_ctl_current.h"
+
+namespace coriander {
+namespace motorctl {
+
+void MotorCtlCurrent::start() {
+  mPidD.P = mParams->getValue<float>(ParamId::MotorCtl_CurrCtl_PidP);
+  mPidD.I = mParams->getValue<float>(ParamId::MotorCtl_CurrCtl_PidI);
+  mPidD.D = mParams->getValue<float>(ParamId::MotorCtl_CurrCtl_PidD);
+  mPidD.output_ramp =
+      mParams->getValue<float>(ParamId::MotorCtl_CurrCtl_PidOutputRamp);
+  mPidD.limit = mParams->getValue<float>(ParamId::MotorCtl_CurrCtl_PidLimit);
+  mPidD.reset();
+
+  mPidQ.P = mParams->getValue<float>(ParamId::MotorCtl_CurrCtl_PidP);
+  mPidQ.I = mParams->getValue<float>(ParamId::MotorCtl_CurrCtl_PidI);
+  mPidQ.D = mParams->getValue<float>(ParamId::MotorCtl_CurrCtl_PidD);
+  mPidQ.output_ramp =
+      mParams->getValue<float>(ParamId::MotorCtl_CurrCtl_PidOutputRamp);
+  mPidQ.limit = mParams->getValue<float>(ParamId::MotorCtl_CurrCtl_PidLimit);
+  mPidQ.reset();
+
+  mDurationTimeout->setDuration(static_cast<uint32_t>(
+      1e6 / mParams->getValue<uint32_t>(ParamId::MotorCtl_CurrCtl_Freq)));
+  
+  mTargetId = mParams->getValue<float>(ParamId::MotorCtl_General_TargetCurrentD_RT);
+  mTargetIq = mParams->getValue<float>(ParamId::MotorCtl_General_TargetCurrentQ_RT);
+
+  // enable sensors, motor
+  mPhaseCurrentEstimator->enable();
+  mDurationEstimator->recordStart();
+  mDurationTimeout->reset();
+  mFocMotorDriver->enable();
+}
+
+void MotorCtlCurrent::stop() {
+  mPhaseCurrentEstimator->disable();
+  mFocMotorDriver->disable();
+}
+
+void MotorCtlCurrent::loop() {
+  const uint32_t maxDurationUs = 1'000;
+  uint32_t durationUs;
+  float errorId, errorIq;
+  float currId, currIq;
+  float outputUd, outputUq;
+
+  mPhaseCurrentEstimator->sync();
+
+  // limit calculation frequency
+  if (mDurationTimeout->expired()) {
+    mDurationTimeout->reset();
+
+    mDurationEstimator->recordStop();
+
+    // limit durationMs
+    durationUs = mDurationEstimator->getDuration();
+    if (durationUs > maxDurationUs) {
+      durationUs = maxDurationUs;
+    }
+
+    // get phase current
+    mPhaseCurrentEstimator->getPhaseCurrent(&currId, &currIq);
+
+    // calculate error
+    errorId = mTargetId - currId;
+    errorIq = mTargetIq - currIq;
+
+    // calculate output
+    outputUd = mPidD(errorId, durationUs * 1.0e-6f);
+    outputUq = mPidQ(errorIq, durationUs * 1.0e-6f);
+
+    // set output
+    mFocMotorDriver->setVoltage(outputUd, outputUq);
+
+    mDurationEstimator->recordStart();
+  }
+}
+
+void MotorCtlCurrent::emergencyStop() { this->stop(); }
+
+bool MotorCtlCurrent::fatalError() { return false; }
+
+void MotorCtlCurrent::setTargetCurrent(float id, float iq) {
+  mTargetId = id;
+  mTargetIq = iq;
+
+  mParams->setValue(ParamId::MotorCtl_General_TargetCurrentD_RT, mTargetId);
+  mParams->setValue(ParamId::MotorCtl_General_TargetCurrentQ_RT, mTargetIq);
+}
+
+}  // namespace motorctl
+}  // namespace coriander
---
+++ app/tests/ut_motor_ctl_current.cc
@@ -0,0 +1,76 @@
+/**
+ * @file ut_motor_ctl_current.cc
+ * @author Savent Gate (savent_gate@outlook.com)
+ * @brief
+ * @date 2023-08-21
+ *
+ * Copyright 2023 savent_gate
+ *
+ */
+#include <gtest/gtest.h>
+
+#include "boost/di.hpp"
+#include "coriander/motorctl/iphase_current_estimator.h"
+#include "coriander/motorctl/motor_ctl_current.h"
+#include "mocks.h"
+
+namespace {
+auto& createInjector() {
+  using boost::di::bind;
+  using testing::mock::MockElecAngleEstimator;
+  using testing::mock::MockParamReqValidator;
+  using testing::mock::MockPhaseCurrentEstimator;
+  using testing::mock::MockSystick;
+  static auto injector = boost::di::make_injector(
+      bind<coriander::IParamReqValidator>.to<MockParamReqValidator>(),
+      bind<coriander::os::ISystick>.to<MockSystick>(),
+      bind<coriander::motorctl::FocMotorDriver>.to<testing::mock::MockFocMotorDriver>(),
+      bind<coriander::motorctl::IPhaseCurrentEstimator>.to<MockPhaseCurrentEstimator>(),
+      bind<coriander::motorctl::IElecAngleEstimator>.to<MockElecAngleEstimator>());
+  return injector;
+}
+}  // namespace
+
+TEST(MotorCtlCurrent, targetMode) {
+  using ID = coriander::base::ParamId;
+  using coriander::base::Property;
+  using testing::_;
+  auto& injector = createInjector();
+  auto param = injector.create<std::shared_ptr<coriander::ParameterBase>>();
+  auto systick = injector.create<std::shared_ptr<testing::mock::MockSystick>>();
+  auto motor =
+      injector.create<std::shared_ptr<testing::mock::MockFocMotorDriver>>();
+  auto currentSensor =
+      injector
+          .create<std::shared_ptr<testing::mock::MockPhaseCurrentEstimator>>();
+
+  param->add(Property{1.0f, ID::MotorCtl_CurrCtl_PidP});
+  param->add(Property{1000.0f, ID::MotorCtl_CurrCtl_PidI});
+  param->add(Property{0.0f, ID::MotorCtl_CurrCtl_PidD});
+  param->add(Property{0.0f, ID::MotorCtl_CurrCtl_PidOutputRamp});
+  param->add(Property{10.0f, ID::MotorCtl_CurrCtl_PidLimit});
+  param->add(Property{1000u, ID::MotorCtl_CurrCtl_Freq});
+  param->add(Property{1.0f, ID::MotorCtl_General_TargetCurrentD_RT});
+  param->add(Property{1.0f, ID::MotorCtl_General_TargetCurrentQ_RT});
+
+  auto motorCtl =
+      injector.create<std::shared_ptr<coriander::motorctl::MotorCtlCurrent>>();
+
+  EXPECT_CALL(*systick, systick_us()).WillRepeatedly(testing::Return(0));
+  EXPECT_CALL(*currentSensor, enable()).Times(1);
+  EXPECT_CALL(*motor, enable()).Times(1);
+  motorCtl->start();
+
+  EXPECT_CALL(*currentSensor, sync()).Times(1);
+  ON_CALL(*currentSensor, getPhaseCurrent(_, _))
+      .WillByDefault(testing::DoAll(testing::SetArgPointee<0>(0.0f),
+                                    testing::SetArgPointee<1>(0.0f)));
+  EXPECT_CALL(*systick, systick_us()).WillRepeatedly(testing::Return(2000));
+  EXPECT_CALL(*motor, setVoltage(1.5f, 1.5f)).Times(1);
+  motorCtl->loop();
+
+
+  EXPECT_CALL(*currentSensor, disable()).Times(1);
+  EXPECT_CALL(*motor, disable()).Times(1);
+  motorCtl->stop();
+}
