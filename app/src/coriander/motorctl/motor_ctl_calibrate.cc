3aca4e648a92d0b50e49edeac0ee5a78326992d8
Savent Gate <savent_gate@outlook.com>
2023-09-03 16:00:07 +0800

    motorctl: introduce calibrate FSM to handle multi-sensor calibration

--- app/src/coriander/motorctl/motor_ctl_calibrate.cc
+++ app/src/coriander/motorctl/motor_ctl_calibrate.cc
@@ -15,36 +15,31 @@ namespace coriander {
 namespace motorctl {
 MotorCtlCalibrate::MotorCtlCalibrate(
     std::shared_ptr<IBldcDriver> motor,
+    std::shared_ptr<IPhaseCurrentEstimator> phaseCurrentEstimator,
     std::shared_ptr<IElecAngleEstimator> elecAngleEstimator,
-    std::shared_ptr<Parameter> param,
-    std::shared_ptr<IBoardEvent> boardEvent, std::shared_ptr<ISystick> systick,
+    std::shared_ptr<Parameter> param, std::shared_ptr<IBoardEvent> boardEvent,
+    std::shared_ptr<ISystick> systick, std::shared_ptr<ILogger> logger,
     std::shared_ptr<IParamReqValidator> paramReqValidator)
     : mMotor(motor),
+      mPhaseCurrentEstimator(phaseCurrentEstimator),
       mElecAngleEstimator(elecAngleEstimator),
       mParam(param),
       mBoardEvent(boardEvent),
       mSystick(systick),
-      mSensorHandler{mElecAngleEstimator} {
+      mLogger(logger),
+      mSensorHandler{phaseCurrentEstimator, mElecAngleEstimator} {
   paramReqValidator->addParamReq(this);
 }
 
 void MotorCtlCalibrate::start() {
-  uint16_t dc;
-
   mSensorHandler.enable();
 
   mCalibrateVoltage =
       mParam->getValue<float>(ParamId::MotorCtl_Calibrate_CaliVoltage);
-  mCalibrateDuration =
-      mParam->getValue<int32_t>(ParamId::MotorCtl_Calibrate_CaliDuration);
   mMotorSupplyVoltage =
       mParam->getValue<float>(ParamId::MotorCtl_MotorDriver_SupplyVoltage);
 
-  dc = static_cast<uint16_t>(UINT16_MAX * mCalibrateVoltage /
-                             mMotorSupplyVoltage);
-  mMotor->enable();
-  mMotor->setPhaseDutyCycle(dc, 0, 0);
-  startTimestamp = mSystick->systick_ms();
+  seekCalibrateItem();
 }
 
 void MotorCtlCalibrate::stop() {
@@ -54,18 +49,81 @@ void MotorCtlCalibrate::stop() {
 
 void MotorCtlCalibrate::loop() {
   uint32_t current = mSystick->systick_ms();
+  bool expired = current > startTimestamp + mCalibrateDuration;
 
   mSensorHandler.sync();
-  mElecAngleEstimator->calibrate();
 
-  if (current > startTimestamp + mCalibrateDuration) {
-    mBoardEvent->raiseEvent(IBoardEvent::Event::CalibrateDone);
+  switch (mState) {
+    case State::Calibrate_Idle:
+      break;
+    case State::Calibrate_PhaseCurrent:
+      mPhaseCurrentEstimator->calibrate();
+      break;
+    case State::Calibrate_ElecAngle:
+      mElecAngleEstimator->calibrate();
+      break;
+  }
+
+  if (expired) {
+    exitState();
+    seekCalibrateItem();
   }
 }
 
 void MotorCtlCalibrate::emergencyStop() { this->stop(); }
 
 bool MotorCtlCalibrate::fatalError() { return false; }
+
+void MotorCtlCalibrate::enterState(State state) {
+  switch (state) {
+    case State::Calibrate_Idle:
+      break;
+    case State::Calibrate_PhaseCurrent:
+      mCalibrateDuration = 256;  // fixed 256ms
+      mMotor->enable();
+      mMotor->setPhaseDutyCycle(0, 0, 0);
+      mLogger->log("Phase current calibration start");
+      break;
+    case State::Calibrate_ElecAngle: {
+      uint16_t dc = static_cast<uint16_t>(UINT16_MAX * mCalibrateVoltage /
+                                          mMotorSupplyVoltage);
+      mCalibrateDuration =
+          mParam->getValue<int32_t>(ParamId::MotorCtl_Calibrate_CaliDuration);
+      mMotor->enable();
+      mMotor->setPhaseDutyCycle(dc, 0, 0);
+      mLogger->log("Electrical angle calibration start");
+    } break;
+  }
+  // start timer
+  startTimestamp = mSystick->systick_ms();
+  mState = state;
+}
+
+void MotorCtlCalibrate::exitState() {
+  switch (mState) {
+    case State::Calibrate_Idle:
+      break;
+    case State::Calibrate_PhaseCurrent:
+      mMotor->disable();
+      mLogger->log("Phase current calibration done");
+      break;
+    case State::Calibrate_ElecAngle:
+      mMotor->disable();
+      mLogger->log("Electrical angle calibration done");
+      break;
+  }
+  mState = State::Calibrate_Idle;
+}
+
+void MotorCtlCalibrate::seekCalibrateItem() {
+  if (mPhaseCurrentEstimator->needCalibrate()) {
+    enterState(State::Calibrate_PhaseCurrent);
+  } else if (mElecAngleEstimator->needCalibrate()) {
+    enterState(State::Calibrate_ElecAngle);
+  } else {
+    mBoardEvent->raiseEvent(IBoardEvent::Event::CalibrateDone);
+  }
+}
 
 }  // namespace motorctl
 }  // namespace coriander
