953580e4b5617d324361327611581dd5f789c88a
Savent Gate <savent_gate@outlook.com>
2023-09-06 22:05:34 +0800

    zephyr: sync sensor in openloop(for debug)

--- app/inc/coriander/motorctl/motor_ctl_openloop.h
+++ app/inc/coriander/motorctl/motor_ctl_openloop.h
@@ -14,7 +14,10 @@
 
 #include "coriander/motorctl/duration_estimator.h"
 #include "coriander/motorctl/foc_motor_driver.h"
+#include "coriander/motorctl/ielec_angle_estimator.h"
 #include "coriander/motorctl/imotorctl.h"
+#include "coriander/motorctl/iphase_current_estimator.h"
+#include "coriander/motorctl/sensor_handler.h"
 #include "coriander/parameter_requirements.h"
 #include "coriander/parameter_requirements_validator.h"
 #include "coriander/parameters.h"
@@ -29,10 +32,15 @@ struct MotorCtlOpenLoop : public IMotorCtl, coriander::IParamReq {
       std::shared_ptr<FocMotorDriver> motor,
       std::shared_ptr<Parameter> parameters,
       std::unique_ptr<DurationExpired> durationEstimator,
+      std::shared_ptr<IPhaseCurrentEstimator> phaseCurrentEstimator,
+      std::shared_ptr<IElecAngleEstimator> elecAngleEstimator,
       std::shared_ptr<IParamReqValidator> paramReqValidator)
       : mFocMotorDriver(motor),
         mParameters(parameters),
-        mDurationTimeout(std::move(durationEstimator)) {
+        mDurationTimeout(std::move(durationEstimator)),
+        mPhaseCurrentEstimator(phaseCurrentEstimator),
+        mElecAngleEstimator(elecAngleEstimator),
+        mSensorHandler{phaseCurrentEstimator, elecAngleEstimator} {
     paramReqValidator->addParamReq(this);
   }
   virtual ~MotorCtlOpenLoop() = default;
@@ -57,6 +65,10 @@ struct MotorCtlOpenLoop : public IMotorCtl, coriander::IParamReq {
   std::shared_ptr<FocMotorDriver> mFocMotorDriver;
   std::shared_ptr<Parameter> mParameters;
   std::unique_ptr<DurationExpired> mDurationTimeout;
+  std::shared_ptr<IPhaseCurrentEstimator> mPhaseCurrentEstimator;
+  std::shared_ptr<IElecAngleEstimator> mElecAngleEstimator;
+
+  SensorHandler mSensorHandler;
 
   float mDutyCycleUd = 0;
   float mCurrentAngle = 0;
