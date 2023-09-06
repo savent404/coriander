a9fb62491cd81a678ee86e51e3d3c80c4b0bf0ef
Savent Gate <savent_gate@outlook.com>
2023-09-06 22:05:34 +0800

    zephyr: sync sensor in openloop(for debug)

--- app/src/coriander/motorctl/motor_ctl_openloop.cc
+++ app/src/coriander/motorctl/motor_ctl_openloop.cc
@@ -9,6 +9,16 @@
  */
 #include "coriander/motorctl/motor_ctl_openloop.h"
 
+#ifndef CONFIG_JSCOPE_ENABLE
+#define CONFIG_JSCOPE_ENABLE 0
+#endif
+
+#if CONFIG_JSCOPE_ENABLE
+#define ATTR_JSCOPE \
+  __attribute__((section(".data.jscope"))) __attribute__((used))
+ATTR_JSCOPE static float _dOpenLoopElecAngle = 0.0f;
+#endif
+
 namespace coriander::motorctl {
 
 void MotorCtlOpenLoop::start() {
@@ -19,15 +29,23 @@ void MotorCtlOpenLoop::start() {
   mDutyCycleUd = outVoltage / supplyVoltage;
   mDurationTimeout->setDuration(1);
   mDurationTimeout->reset();
+  mSensorHandler.enable();
   mFocMotorDriver->enable();
 }
 
-void MotorCtlOpenLoop::stop() { mFocMotorDriver->disable(); }
+void MotorCtlOpenLoop::stop() {
+  mFocMotorDriver->disable();
+  mSensorHandler.enable();
+}
 
 void MotorCtlOpenLoop::loop() {
   if (mDurationTimeout->expired()) {
+    mSensorHandler.sync();
     mDurationTimeout->reset();
     mFocMotorDriver->setVoltageNoSensor(mDutyCycleUd, 0, mCurrentAngle);
+#if CONFIG_JSCOPE_ENABLE
+    _dOpenLoopElecAngle = mCurrentAngle;
+#endif
     mCurrentAngle += 0.5f;
     if (mCurrentAngle > 360.0f) {
       mCurrentAngle -= 360.0f;
